076958ac3ccf884579a2e11127e90a4c2907597f
saulram <saul.ramirez@disolutionsmx.com>
2020-07-21 22:11:17 -0500

    Provider separado

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,14 @@
 import 'dart:io';
-
 import 'package:easy_alert/easy_alert.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_simple_dependency_injection/injector.dart';
-import 'package:litadesarrollos/src/pages/account/services/expenses_service.dart';
-import 'package:litadesarrollos/src/pages/bazaar/services/bazaar_service.dart';
-import 'package:litadesarrollos/src/pages/benefits/services/benefits_service.dart';
-import 'package:litadesarrollos/src/pages/chat/services/chat_service.dart';
 import 'package:litadesarrollos/src/pages/chat/utils/app_initializer.dart';
 import 'package:litadesarrollos/src/pages/chat/utils/dependency_injection.dart';
-import 'package:litadesarrollos/src/pages/common_areas/services/common_area_service.dart';
-import 'package:litadesarrollos/src/pages/directorio/services/directory_service.dart';
-import 'package:litadesarrollos/src/pages/directorio/services/personal_service.dart';
-import 'package:litadesarrollos/src/pages/documents/services/documents_service.dart';
-import 'package:litadesarrollos/src/pages/inicio/services/preference_service.dart';
-import 'package:litadesarrollos/src/pages/lost_found/services/lost_found_service.dart';
-import 'package:litadesarrollos/src/pages/maintenance/services/mto_service.dart';
-import 'package:litadesarrollos/src/pages/news/services/news_service.dart';
-import 'package:litadesarrollos/src/pages/notifications/services/notifications_service.dart';
-import 'package:litadesarrollos/src/pages/programedvisits/services/visits_service.dart';
 import 'package:litadesarrollos/src/pages/root_page.dart';
-import 'package:litadesarrollos/src/pages/votesandpolls/services/polls_service.dart';
-import 'package:litadesarrollos/src/pages/votesandpolls/services/votes_service.dart';
-import 'package:litadesarrollos/src/pages/wall/services/wall_service.dart';
-import 'package:litadesarrollos/src/services/graphql_terms_service.dart';
-import 'package:litadesarrollos/src/services/login_service.dart';
+import 'package:litadesarrollos/src/utils/provider_list.dart';
 import 'package:litadesarrollos/src/utils/routes.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/services.dart';
-import 'src/services/login_service.dart';
 Injector injector;
 
 class MyHttpOverrides extends HttpOverrides {
@@ -68,101 +48,7 @@ class _MyAppState extends State<MyApp> {
       DeviceOrientation.portraitDown,
     ]);
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => LoginService(),
-        ),
-        ChangeNotifierProxyProvider<LoginService, TermsService>(
-          create: (_) => TermsService(),
-          update: (context, loginServce, termService) =>
-              termService..update(loginServce.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, DirectoryService>(
-          create: (_) => DirectoryService(),
-          update: (context, loginServce, directoryService) =>
-              directoryService..update(loginServce.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, PersonalService>(
-          create: (_) => PersonalService(),
-          update: (context, loginServce, personalService) =>
-              personalService..update(loginServce.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, VisitService>(
-          create: (_) => VisitService(),
-          update: (context, loginServce, visitService) =>
-              visitService..update(loginServce.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, MtoService>(
-          create: (_) => MtoService(),
-          update: (context, loginServce, mtoService) =>
-              mtoService..update(loginServce.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, NewsService>(
-          create: (_) => NewsService(),
-          update: (context, loginServce, newsService) =>
-              newsService..update(loginServce.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, DocService>(
-          create: (_) => DocService(),
-          update: (context, loginServce, docsService) =>
-              docsService..update(loginServce.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, NotificationService>(
-          create: (_) => NotificationService(),
-          update: (context, loginServce, notificationsService) =>
-              notificationsService..update(loginServce.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, VoteService>(
-          create: (_) => VoteService(),
-          update: (context, loginService, voteService) =>
-              voteService..update(loginService.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, PollService>(
-          create: (_) => PollService(),
-          update: (context, loginService, pollService) =>
-          pollService..update(loginService.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, CommonService>(
-          create: (_) => CommonService(),
-          update: (context, loginService, commonService) =>
-          commonService..update(loginService.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, WallService>(
-          create: (_) => WallService(),
-          update: (context, loginService, commonService) =>
-          commonService..update(loginService.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, LostService>(
-          create: (_) => LostService(),
-          update: (context, loginService, lostService) =>
-          lostService..update(loginService.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, BazaarService>(
-          create: (_) => BazaarService(),
-          update: (context, loginService, bazaarService) =>
-          bazaarService..update(loginService.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, ChatService>(
-          create: (_) => ChatService(),
-          update: (context, loginService, bazaarService) =>
-          bazaarService..update(loginService.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, BenefitsService>(
-          create: (_) => BenefitsService(),
-          update: (context, loginService, bazaarService) =>
-          bazaarService..update(loginService.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, ExpensesService>(
-          create: (_) => ExpensesService(),
-          update: (context, loginService, bazaarService) =>
-          bazaarService..update(loginService.loginResult),
-        ),
-        ChangeNotifierProxyProvider<LoginService, PrefService>(
-          create: (_) => PrefService(),
-          update: (context, loginService, prefsService) =>
-          prefsService..update(loginService.loginResult),
-        ),
-      ],
+      providers: providerList,
       child: AlertProvider(
         child: MaterialApp(
           navigatorKey: navigatorKey,
---
+++ lib/src/utils/provider_list.dart
@@ -0,0 +1,122 @@
+/*
+Lista de Providers usados en la app.
+
+ */
+
+import 'package:litadesarrollos/src/pages/account/services/expenses_service.dart';
+import 'package:litadesarrollos/src/pages/bazaar/services/bazaar_service.dart';
+import 'package:litadesarrollos/src/pages/benefits/services/benefits_service.dart';
+import 'package:litadesarrollos/src/pages/chat/services/chat_service.dart';
+import 'package:litadesarrollos/src/pages/common_areas/services/common_area_service.dart';
+import 'package:litadesarrollos/src/pages/directorio/services/directory_service.dart';
+import 'package:litadesarrollos/src/pages/directorio/services/personal_service.dart';
+import 'package:litadesarrollos/src/pages/documents/services/documents_service.dart';
+import 'package:litadesarrollos/src/pages/inicio/services/preference_service.dart';
+import 'package:litadesarrollos/src/pages/lost_found/services/lost_found_service.dart';
+import 'package:litadesarrollos/src/pages/maintenance/services/mto_service.dart';
+import 'package:litadesarrollos/src/pages/news/services/news_service.dart';
+import 'package:litadesarrollos/src/pages/notifications/services/notifications_service.dart';
+import 'package:litadesarrollos/src/pages/programedvisits/services/visits_service.dart';
+import 'package:litadesarrollos/src/pages/votesandpolls/services/polls_service.dart';
+import 'package:litadesarrollos/src/pages/votesandpolls/services/votes_service.dart';
+import 'package:litadesarrollos/src/pages/wall/services/wall_service.dart';
+import 'package:litadesarrollos/src/services/graphql_terms_service.dart';
+import 'package:litadesarrollos/src/services/login_service.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+List<SingleChildWidget> providerList = [
+  ChangeNotifierProvider(
+    create: (_) => LoginService(),
+  ),
+  ChangeNotifierProxyProvider<LoginService, TermsService>(
+    create: (_) => TermsService(),
+    update: (context, loginServce, termService) =>
+    termService..update(loginServce.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, DirectoryService>(
+    create: (_) => DirectoryService(),
+    update: (context, loginServce, directoryService) =>
+    directoryService..update(loginServce.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, PersonalService>(
+    create: (_) => PersonalService(),
+    update: (context, loginServce, personalService) =>
+    personalService..update(loginServce.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, VisitService>(
+    create: (_) => VisitService(),
+    update: (context, loginServce, visitService) =>
+    visitService..update(loginServce.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, MtoService>(
+    create: (_) => MtoService(),
+    update: (context, loginServce, mtoService) =>
+    mtoService..update(loginServce.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, NewsService>(
+    create: (_) => NewsService(),
+    update: (context, loginServce, newsService) =>
+    newsService..update(loginServce.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, DocService>(
+    create: (_) => DocService(),
+    update: (context, loginServce, docsService) =>
+    docsService..update(loginServce.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, NotificationService>(
+    create: (_) => NotificationService(),
+    update: (context, loginServce, notificationsService) =>
+    notificationsService..update(loginServce.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, VoteService>(
+    create: (_) => VoteService(),
+    update: (context, loginService, voteService) =>
+    voteService..update(loginService.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, PollService>(
+    create: (_) => PollService(),
+    update: (context, loginService, pollService) =>
+    pollService..update(loginService.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, CommonService>(
+    create: (_) => CommonService(),
+    update: (context, loginService, commonService) =>
+    commonService..update(loginService.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, WallService>(
+    create: (_) => WallService(),
+    update: (context, loginService, commonService) =>
+    commonService..update(loginService.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, LostService>(
+    create: (_) => LostService(),
+    update: (context, loginService, lostService) =>
+    lostService..update(loginService.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, BazaarService>(
+    create: (_) => BazaarService(),
+    update: (context, loginService, bazaarService) =>
+    bazaarService..update(loginService.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, ChatService>(
+    create: (_) => ChatService(),
+    update: (context, loginService, bazaarService) =>
+    bazaarService..update(loginService.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, BenefitsService>(
+    create: (_) => BenefitsService(),
+    update: (context, loginService, bazaarService) =>
+    bazaarService..update(loginService.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, ExpensesService>(
+    create: (_) => ExpensesService(),
+    update: (context, loginService, bazaarService) =>
+    bazaarService..update(loginService.loginResult),
+  ),
+  ChangeNotifierProxyProvider<LoginService, PrefService>(
+    create: (_) => PrefService(),
+    update: (context, loginService, prefsService) =>
+    prefsService..update(loginService.loginResult),
+  ),
+];
